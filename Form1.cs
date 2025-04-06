30ffc05ec15d3bbcd2953ef94095031c776c1b89
csokanandor95 <csokanandor77@gmail.com>
2025-04-06 17:14:04 +0200

    Checkbox logic and button activation logic added

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,16 @@ namespace Win_Form_App
         {
             InitializeComponent(); //lefutnak a fõ kódfájlból ez ehhez tartozó komponensek
             //tehetek hozzá a konstruktorba mást is, de fontos h az InitializeC. után jöjjön
+            checkBox1.CheckedChanged += CheckBoxes_CheckedChanged;
+            checkBox2.CheckedChanged += CheckBoxes_CheckedChanged;
+            checkBox3.CheckedChanged += CheckBoxes_CheckedChanged;
+            checkBox4.CheckedChanged += CheckBoxes_CheckedChanged;
+            checkBox5.CheckedChanged += CheckBoxes_CheckedChanged;
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            UpdateButtons(); //kezdeti állapot beállítása
         }
 
         private void button1_Click(object sender, EventArgs e) //eseménykezelõ metódus
@@ -17,5 +27,24 @@ namespace Win_Form_App
         {
             Result.Text = "QC Rejected";
         }
+
+        private void CheckBoxes_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateButtons();
+        }
+
+        private void UpdateButtons()
+        {
+            bool allChecked = checkBox1.Checked &&
+                              checkBox2.Checked &&
+                              checkBox3.Checked &&
+                              checkBox4.Checked &&
+                              checkBox5.Checked;
+
+            button1.Enabled = allChecked;
+            button2.Enabled = !allChecked;
+        }
+
+
     }
 }
