20fc56c1662f19d3e57fa15e2d76a47e1d359a82
csokanandor95 <csokanandor77@gmail.com>
2025-04-06 17:46:05 +0200

    Added result changed text: "Under QC"

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.CodeDom.Compiler;
+
 namespace Win_Form_App
 {
     public partial class Form1 : Form
@@ -29,6 +31,7 @@ namespace Win_Form_App
             checkBox5.Checked = false;
 
             UpdateButtons();
+            ResultChangedEmpty();
         }
 
         private void button1_Click(object sender, EventArgs e) //eseménykezelõ metódus
@@ -55,9 +58,13 @@ namespace Win_Form_App
                               checkBox5.Checked;
 
             button1.Enabled = allChecked;
-            button2.Enabled = !allChecked;
+            button2.Enabled = !allChecked;                        
         }
 
+        private void ResultChangedEmpty()
+        {
+            Result.Text = "Under QC";
+        }
 
     }
 }
