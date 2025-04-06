acbdc99c199b0466b6084d27555dbb48f9827bcf
csokanandor95 <csokanandor77@gmail.com>
2025-04-06 17:37:41 +0200

    In case of assay type change in dropdown list, the checkboxes reset

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,23 @@ namespace Win_Form_App
             checkBox4.CheckedChanged += CheckBoxes_CheckedChanged;
             checkBox5.CheckedChanged += CheckBoxes_CheckedChanged;
 
+            Assay.SelectedIndexChanged += Assay_SelectedIndexChanged;
+
             button1.Enabled = false;
             button2.Enabled = true;
 
             UpdateButtons(); //kezdeti állapot beállítása
+        }
+
+        private void Assay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+
+            UpdateButtons();
         }
 
         private void button1_Click(object sender, EventArgs e) //eseménykezelõ metódus
